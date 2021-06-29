626027fabdc689cfdf15c0a67995126216a9710e
Leo <57889616+ThisIsTenou@users.noreply.github.com>
2021-06-29 22:24:18 +0200

    Update async.sh

--- async.sh
+++ async.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # Runs a shell command asynchronously.
 
-nohup /bin/sh /vmfs/volumes/$(hostname | cut -d '.' -f1)_datastore-local-1/000-scripts/esxidown.sh > /dev/null 2>&1 &
+nohup /bin/sh /vmfs/volumes/yourdatastore/esxidown.sh > /dev/null 2>&1 &
